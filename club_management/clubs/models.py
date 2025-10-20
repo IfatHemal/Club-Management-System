d59d37aa881e1d9fafad563ecba035b9efc3cb30
IfatHemal <ifathemal9@gmail.com>
2025-10-21 04:46:23 +0600

    Added User Model Class

--- club_management/clubs/models.py
+++ club_management/clubs/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    class Role(models.TextChoices):
+        HEAD_ADMIN = 'HEAD', 'Head Admin'
+        CLUB_ADMIN = 'CLUB', 'Club Admin'
+        NORMAL = 'USER', 'Normal User'
+
+    role = models.CharField(max_length=10, choices=Role.choices, default=Role.NORMAL)
+
+    def is_head_admin(self):
+        return self.role == self.Role.HEAD_ADMIN
+
+    def is_club_admin(self):
+        return self.role == self.Role.CLUB_ADMIN
