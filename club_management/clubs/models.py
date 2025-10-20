bd294237c67fd9df57440098f48547d28be5f629
IfatHemal <ifathemal9@gmail.com>
2025-10-21 05:10:44 +0600

    Member model class added

--- club_management/clubs/models.py
+++ club_management/clubs/models.py
@@ -25,3 +25,40 @@ class Club(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Member(models.Model):
+    club = models.ForeignKey(Club, on_delete=models.CASCADE, related_name='members')
+    full_name = models.CharField(max_length=120)
+    profile_pic = models.ImageField(upload_to='members/', blank=True, null=True)
+    ROLE_CHOICES = [
+        ('Convenor', 'Convenor'),
+        ('Co-Convenor', 'Co-Convenor'),
+        ('Advisor', 'Advisor'),
+        ('Co-Advisor', 'Co-Advisor'),
+        ('President', 'President'),
+        ('Vice President', 'Vice President'),
+        ('Secretary', 'Secretary'),
+        ('Selector', 'Selector'),
+        ('General Secretary', 'General Secretary'),
+        ('Chief Executive', 'Chief Executive'),
+        ('Executive', 'Executive'),
+        ('Junior Executive', 'Junior Executive'),
+        ('Treasurer', 'Treasurer'),
+        ('Coordinator', 'Coordinator'),
+        ('Event Coordinator', 'Event Coordinator'),
+        ('Media & Publications', 'Media & Publications'),
+        ('Creative Graphics Executive', 'Creative Graphics Executive'),
+        ('Seeker', 'Seeker'),
+        ('Controller', 'Controller'),
+        ('Manager', 'Manager'),
+        ('Infrastructure Manager', 'Infrastructure Manager'),
+        ('Department Representative', 'Department Representative'),
+        ('Member', 'Member'),
+
+    ]
+    role = models.CharField(max_length=40, choices=ROLE_CHOICES, default='Member')
+    email = models.EmailField()
+
+    def __str__(self):
+        return f"{self.full_name} — {self.club.name}"
