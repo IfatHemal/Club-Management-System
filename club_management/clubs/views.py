008b45e2da3b194a5d99fd02446ae3f757a2eddc
IfatHemal <ifathemal9@gmail.com>
2025-10-21 19:53:10 +0600

    Added signupview class for sigining up a user

--- club_management/clubs/views.py
+++ club_management/clubs/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views import View
+from .forms import SignUpForm
 
-# Create your views here.
+class SignUpView(View):
+    template_name = 'clubs/signup.html'
+
+    def get(self, request):
+        form = SignUpForm()
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request):
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+
+            user = form.save(commit=False)
+            user.is_active = True
+            user.save()
+            login(request, user)
+            return redirect('clubs:home')
+        return render(request, self.template_name, {'form': form})
